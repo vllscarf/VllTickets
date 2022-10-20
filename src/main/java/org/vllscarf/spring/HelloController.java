16806b347624f9b583f409da0426bda17ee66693
vllscarf <99333946+vllscarf@users.noreply.github.com>
2022-10-20 21:17:14 +0300

    Initial commit

---
+++ src/main/java/org/vllscarf/spring/HelloController.java
@@ -0,0 +1,13 @@
+package org.vllscarf.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "isa";
+    }
+}
